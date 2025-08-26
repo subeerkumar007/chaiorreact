1481b2d4316ffba6d13edd030cf6cd137445c645
subeerkumar007 <subeerk491@gmail.com>
2025-08-26 22:43:13 +0530

    10PM

---
+++ Js_chai/conversion_op.js
@@ -0,0 +1,19 @@
+//coversion operator in Js
+// let score= 45
+// console.log(typeof(score));
+
+//  let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//*********************operation***********************
+
+// let str1 ="Subeer" 
+// let str2 = " kumar"
+
+// let str3 = str1 + str2
+// console.log(str3);
+console.log("1"+2+3)
