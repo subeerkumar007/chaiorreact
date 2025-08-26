9ceadf8a0dfada982817a648f04e9dec2cd0e870
subeerkumar007 <subeerk491@gmail.com>
2025-08-26 21:45:55 +0530

    latest

---
+++ Js_chai/data.js
@@ -0,0 +1,12 @@
+//console.log("DataTypes")
+
+/* null 
+ undefined 
+ boolean 
+ int 
+ string
+ object 
+*/
+console.log(typeof "Subeer")
+console.log(typeof 234)
+console.log(typeof true)
--- Js_chai/index.js
+++ Js_chai/index.js
@@ -1,1 +1,7 @@
-console.log("JavaScript staring...")
+//console.log("JavaScript staring...")
+const accountID= 1234
+let accountEmail ="test123@gmail.com"
+var accountPassword = "1233"
+let accounState;
+
+console.table([accountID,accountEmail,accountPassword,accounState])
