6a6893a61eb592824773f3d6f8736399a7e8a16c
subeerkumar007 <subeerk491@gmail.com>
2025-08-28 20:23:52 +0530

    intial

---
+++ basic_js_02/Array.js
@@ -0,0 +1,7 @@
+//Array used in JavaScript
+let fruits =["banana","apple","gwava"]
+fruits.length = 10;
+console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+console.log(fruits.length); // 10
+console.log(fruits[8]); // undefined
