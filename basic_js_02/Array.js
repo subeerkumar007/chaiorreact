6bace4886cc2b87bf74f5bb06f4b8642e26083b0
subeerkumar007 <subeerk491@gmail.com>
2025-08-28 21:11:39 +0530

    Fo

--- basic_js_02/Array.js
+++ basic_js_02/Array.js
@@ -1,7 +1,12 @@
-//Array used in JavaScript
-let fruits =["banana","apple","gwava"]
-fruits.length = 10;
-console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
-console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
-console.log(fruits.length); // 10
-console.log(fruits[8]); // undefined
+// //Array used in JavaScript
+// let fruits =["banana","apple","gwava"]
+// fruits.length = 10;
+// console.log(fruits); // ['banana', 'apple', 'peach', empty x 2, 'mango', empty x 4]
+// console.log(Object.keys(fruits)); // ['0', '1', '2', '5']
+// console.log(fruits.length); // 10
+// console.log(fruits[8]); // undefined
+
+let cars = ["bmw","mercedes","lembo"]
+cars.push("sonala")
+console.log(cars)
+//console.log(newReverse)
