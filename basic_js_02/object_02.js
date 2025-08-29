b1a8947f1a9edb8e75463b8e1c5307712bc4530f
subeerkumar007 <subeerk491@gmail.com>
2025-08-29 22:08:21 +0530

    Do

--- basic_js_02/object_02.js
+++ basic_js_02/object_02.js
@@ -1,13 +1,49 @@
-const tinderUser = {}
-tinderUser.name = 'Mohan' 
-tinderUser.age = 22
-tinderUser.isLoggedIn = false
+// const tinderUser = {}
+// tinderUser.name = 'Mohan' 
+// tinderUser.age = 22
+// tinderUser.isLoggedIn = false
 
-const regularUser = {
-    email : "Mohan@gmail.com",
-    fullname : {
-        FirstName : "Mohan",
-        LastName : "kumar"
-    }
+// const regularUser = {
+//     email : "Mohan@gmail.com",
+//     fullname : {
+//         FirstName : "Mohan",
+//         LastName : "kumar"
+//     }
+// }
+// console.log(regularUser.fullname?.userfullname.firstname);
+// const Emp = {
+//     name: "Rohit",
+//     //age : 45,
+//     City : "Delhi"
+
+// }
+// //console.table(Emp)
+// const  freezeObj = Object.freeze(Emp)
+// freezeObj.age = 56
+// console.log(this.age)
+
+//keys 
+
+// const obj = {
+//     a : "BMW",
+//     b : "Audi",
+//     c : "Tesla",
+// };
+// console.log(Object.keys(obj));
+// const course = {
+//     name :"JavaScript",
+//     price : 999,
+//     courseInstructor : "Mohan",
+//     courseContent : {
+//         chapter1 : "HTML",
+//         chapter2 : "CSS",
+//         chapter3 : "JS"
+//     }
+// }
+// const {courseContent:{chapter1}} = course 
+// console.log({chapter1});
+{
+    name: "Chacha chaudhary"
+    age : 54 
+    city : "Delhi"
 }
-console.log(regularUser.fullname?.userfullname.firstname);
