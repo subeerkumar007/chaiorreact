93c5878f6983f5a75c1eb40c8f62d0912e870d2d
subeerkumar007 <subeerk491@gmail.com>
2025-08-31 08:59:01 +0530

    latest

--- basic_js_02/arrow.js
+++ basic_js_02/arrow.js
@@ -1,9 +1,28 @@
-const user ={
-    name : "chacha",
-    price : 999,
+// const user ={
+//     name : "chacha",
+//     price : 999,
 
-    welcomeMessage : function () {
-        console.log(`Welcome to the new user ${this.name} this is price ${this.price}`);
-    }
-}
-user.welcomeMessage()
+//     welcomeMessage : function () {
+//         console.log(`Welcome to the new user ${this.name} this is price ${this.price}`);
+//     }
+// }
+// user.welcomeMessage()
+
+
+// function chai () {
+//     console.log(this)
+// }
+
+// chai()
+
+// const addTwo = (num1 , num2) =>  (num1+num2) 
+
+// console.log(addTwo(2,3))
+
+(function one () {
+    console.log('DB connected')
+}) (); 
+( (name) => {
+    console.log(`TWO DB connnected, ${name}`);
+} ) ('Human');
+// IIFE => Immediately invoked function expression 
