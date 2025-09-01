d7d53d4732bedc8f28b943d2cbf828498bce67d2
subeerkumar007 <subeerk491@gmail.com>
2025-09-01 21:17:49 +0530

    initi

---
+++ basic_js_02/control.js
@@ -0,0 +1,50 @@
+// let = 1000
+
+// if (let > 2000) {
+//     console.log("Greater than 2000")
+//       }   
+//         else {console.log("less than 2000")
+//         }
+// const month =  "march"
+// switch (month) {
+//     case 1: console.loh("jan")
+//      break;
+   
+//     case 2: console.log("fevb")
+//     break;
+
+//     case 3: console.log("march")
+//     break;
+
+//     case 4: console.log("april")
+//     break;
+
+// }
+
+//for Loop 
+// const array = [1,2,3,4,5,6,7,8,9,10]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+// console.log(array)
+
+// for (i = 0; i<= 10; i++) {
+//     const index = i;
+//     console.log(i)
+// }
+
+// for (let i = 0;i <=10; i++) {
+//     for (let j = 0; j <=10;j++) {
+//         console.log(i + '*' + j +'=' +i*j)
+//     }
+// }
+const arr = [1,2,3,4,5,6]
+for (const num of arr) {
+     console.log(num)
+}
+
+// for ( int in arr) {
+//     console.log(int)
+// }
+
