638cdc60d4367b3c8fff6f6208464fec44bbb86a
subeerkumar007 <subeerk491@gmail.com>
2025-09-02 21:17:08 +0530

    todays

---
+++ basic_js_02/map.js
@@ -0,0 +1,33 @@
+// const map = new Map();
+// map.set('IND',"India");
+// map.set('USA',"United States of America");
+// map.set('UK',"United Kingdom");
+// map.set('AUS',"Australia");
+
+// //console.log(map)
+// for (`the all ${key} of map of ${Map}`) {
+//     console.log(Map);
+// }
+
+// const subjects = {
+//          C : 'C Programming',
+//          CPP : 'C++ Programming',   
+//             JS : 'JavaScript',
+//             REACT : 'ReactJS'
+// }
+
+// for (const key in subjects) {
+//     console.log(`the key is ${key} and the value is ${subjects[key]}`);
+// }
+     
+
+ const myCoding = [
+  
+      {   c : 'C Programming',
+        cpp : 'C++ Programming',
+        js : 'JavaScript',
+        react : 'ReactJS'
+ }
+]
+
+myCoding.forEach((item) => { console.log(item); });
